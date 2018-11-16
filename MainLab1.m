0608fd8dfe9df6478fd3e2fa86a851818bd38cce
Francesco W <fwanderlingh@gmail.com>
2018-11-16 12:47:57 +0100

    Updated with versor lemma and inserted biTri

--- MainLab1.m
+++ MainLab1.m
@@ -1,43 +1,44 @@
 %%MOCOM LAB1
 
-clc
-clear
-close all
+%clc
+%clear
+%close all
 
-biTri(1,1,1) =  ; biTri(1,2,1) = ; biTri(1,3,1) = ; biTri(1,4,1) = ;
-biTri(2,1,1) =  ; biTri(2,2,1) = ; biTri(2,3,1) = ; biTri(2,4,1) = ;
-biTri(3,1,1) =  ; biTri(3,2,1) = ; biTri(3,3,1) = ; biTri(3,4,1) = ;
-biTri(4,1,1) =  ; biTri(4,2,1) = ; biTri(4,3,1) = ; biTri(4,4,1) = ;
+biTri(1,1,1) = 1; biTri(1,2,1) = 0; biTri(1,3,1) = 0; biTri(1,4,1) = 0;
+biTri(2,1,1) = 0; biTri(2,2,1) = 1; biTri(2,3,1) = 0; biTri(2,4,1) = 0;
+biTri(3,1,1) = 0; biTri(3,2,1) = 0; biTri(3,3,1) = 1; biTri(3,4,1) = 0.175;
+biTri(4,1,1) = 0; biTri(4,2,1) = 0; biTri(4,3,1) = 0; biTri(4,4,1) = 1;
 
-biTri(1,1,2) = ; biTri(1,2,2) = ;  biTri(1,3,2) = ; biTri(1,4,2) = ;
-biTri(2,1,2) = ;  biTri(2,2,2) = ;  biTri(2,3,2) = ; biTri(2,4,2) = ;
-biTri(3,1,2) = ;  biTri(3,2,2) = ;  biTri(3,3,2) = ; biTri(3,4,2) = ;
-biTri(4,1,2) = ;  biTri(4,2,2) = ;  biTri(4,3,2) = ; biTri(4,4,2) = ;
+biTri(1,1,2) = -1; biTri(1,2,2) = 0;  biTri(1,3,2) = 0; biTri(1,4,2) = 0;
+biTri(2,1,2) = 0;  biTri(2,2,2) = 0;  biTri(2,3,2) = 1; biTri(2,4,2) = 0;
+biTri(3,1,2) = 0;  biTri(3,2,2) = 1;  biTri(3,3,2) = 0; biTri(3,4,2) = 0.108;
+biTri(4,1,2) = 0;  biTri(4,2,2) = 0;  biTri(4,3,2) = 0; biTri(4,4,2) = 1;
 
-biTri(1,1,3) = ; biTri(1,2,3) = ;  biTri(1,3,3) = ;  biTri(1,4,3) = ;
-biTri(2,1,3) = ; biTri(2,2,3) = ;  biTri(2,3,3) = ; biTri(2,4,3) = ;
-biTri(3,1,3) = ; biTri(3,2,3) = ; biTri(3,3,3) = ;  biTri(3,4,3) = ;
-biTri(4,1,3) = ; biTri(4,2,3) = ;  biTri(4,3,3) = ;  biTri(4,4,3) = ;
+biTri(1,1,3) = 0;  biTri(1,2,3) = 0; biTri(1,3,3) = 1;  biTri(1,4,3) = 0.105;
+biTri(2,1,3) = -1; biTri(2,2,3) = 0; biTri(2,3,3) = 0;  biTri(2,4,3) = 0;
+biTri(3,1,3) = 0;  biTri(3,2,3) = -1; biTri(3,3,3) = 0; biTri(3,4,3) = 0;
+biTri(4,1,3) = 0;  biTri(4,2,3) = 0; biTri(4,3,3) = 0;  biTri(4,4,3) = 1;
 
-biTri(1,1,4) = ; biTri(1,2,4) = ;  biTri(1,3,4) = ; biTri(1,4,4) = ;
-biTri(2,1,4) = ; biTri(2,2,4) = ;  biTri(2,3,4) = ; biTri(2,4,4) = ;
-biTri(3,1,4) = ; biTri(3,2,4) = ; biTri(3,3,4) = ; biTri(3,4,4) = ;
-biTri(4,1,4) = ; biTri(4,2,4) = ;  biTri(4,3,4) = ; biTri(4,4,4) = ;
+biTri(1,1,4) = 0;  biTri(1,2,4) = 1;  biTri(1,3,4) = 0;  biTri(1,4,4) = -0.1455;
+biTri(2,1,4) = 0;  biTri(2,2,4) = 0;  biTri(2,3,4) = -1; biTri(2,4,4) = 0;
+biTri(3,1,4) = -1; biTri(3,2,4) = 0;  biTri(3,3,4) = 0;  biTri(3,4,4) = 0.3265;
+biTri(4,1,4) = 0;  biTri(4,2,4) = 0;  biTri(4,3,4) = 0;  biTri(4,4,4) = 1;
 
-biTri(1,1,5) = ; biTri(1,2,5) = ; biTri(1,3,5) = ;  biTri(1,4,5) = ;
-biTri(2,1,5) = ; biTri(2,2,5) = ; biTri(2,3,5) = ; biTri(2,4,5) = ;
-biTri(3,1,5) = ; biTri(3,2,5) = ; biTri(3,3,5) = ;  biTri(3,4,5) = ;
-biTri(4,1,5) = ; biTri(4,2,5) = ; biTri(4,3,5) = ;  biTri(4,4,5) = ;
+biTri(1,1,5) = 0; biTri(1,2,5) = 0;  biTri(1,3,5) = 1; biTri(1,4,5) = 0.095;
+biTri(2,1,5) = 0; biTri(2,2,5) = -1; biTri(2,3,5) = 0; biTri(2,4,5) = 0;
+biTri(3,1,5) = 1; biTri(3,2,5) = 0;  biTri(3,3,5) = 0; biTri(3,4,5) = 0;
+biTri(4,1,5) = 0; biTri(4,2,5) = 0;  biTri(4,3,5) = 0; biTri(4,4,5) = 1;
 
-biTri(1,1,6) = ; biTri(1,2,6) = ;  biTri(1,3,6) = ; biTri(1,4,6) = ;
-biTri(2,1,6) = ; biTri(2,2,6) = ;  biTri(2,3,6) = ; biTri(2,4,6) = ;
-biTri(3,1,6) = ; biTri(3,2,6) = ; biTri(3,3,6) = ; biTri(3,4,6) = ;
-biTri(4,1,6) = ; biTri(4,2,6) = ;  biTri(4,3,6) = ; biTri(4,4,6) = ;
+biTri(1,1,6) = 0; biTri(1,2,6) = 0;  biTri(1,3,6) = 1; biTri(1,4,6) = 0;
+biTri(2,1,6) = 0; biTri(2,2,6) = -1; biTri(2,3,6) = 0; biTri(2,4,6) = 0;
+biTri(3,1,6) = 1; biTri(3,2,6) = 0;  biTri(3,3,6) = 0; biTri(3,4,6) = 0.325;
+biTri(4,1,6) = 0; biTri(4,2,6) = 0;  biTri(4,3,6) = 0; biTri(4,4,6) = 1;
 
-biTri(1,1,7) = ; biTri(1,2,7) = ; biTri(1,3,7) = ;  biTri(1,4,7) = ;
-biTri(2,1,7) = ; biTri(2,2,7) = ; biTri(2,3,7) = ; biTri(2,4,7) = ;
-biTri(3,1,7) = ; biTri(3,2,7) = ; biTri(3,3,7) = ;  biTri(3,4,7) = ;
-biTri(4,1,7) = ; biTri(4,2,7) = ; biTri(4,3,7) = ;  biTri(4,4,7) = ;
+biTri(1,1,7) = 0; biTri(1,2,7) = 0;  biTri(1,3,7) = 1; biTri(1,4,7) = 0.132;
+biTri(2,1,7) = 0; biTri(2,2,7) = -1; biTri(2,3,7) = 0; biTri(2,4,7) = 0;
+biTri(3,1,7) = 1; biTri(3,2,7) = 0;  biTri(3,3,7) = 0; biTri(3,4,7) = 0;
+biTri(4,1,7) = 0; biTri(4,2,7) = 0;  biTri(4,3,7) = 0; biTri(4,4,7) = 1;
+
 
 %-------------------MOVE----------------------%
 
---
+++ include/VersorLemma.m
@@ -0,0 +1,11 @@
+%% VersorLemma function
+% inputs:
+% - r1: the first rotation matrix
+% - r2: the second rotation matrix
+% output:
+% - c: the Vect3 representing the axis around which r1 should rotate to reach r2, where its modulus is the angle
+
+function c = VersorLemma(r1, r2)
+%TODO
+end
+
